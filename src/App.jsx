c151f59d8cb869d2d67958ff5afac4d0154113a1
kelvin <kelvinmutugi336@gmil.com>
2024-08-01 16:52:06 +0300

    sets up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;

d370e2e06a0a4e556bbd44f2341b11761a318a2e
kelvin <kelvinmutugi336@gmil.com>
2024-08-02 09:06:14 +0300

    adds footer and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Home />
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </>

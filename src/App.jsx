de8b5db0ceba6d6bc8110ecb58e23d027a66d0e9
kelvin <kelvinmutugi336@gmil.com>
2024-08-02 13:33:46 +0300

    signup and logins components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route
             path="/home"
             element={

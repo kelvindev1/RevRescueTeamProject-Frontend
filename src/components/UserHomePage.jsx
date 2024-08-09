6e83382fdf45aa7b37ddba8cda67d1f8b25dc954
lowellteddy <lowellteddy88@gmail.com>
2024-08-09 13:52:46 +0300

    corrected the user homepage and added its navigation

--- src/components/UserHomePage.jsx
+++ src/components/UserHomePage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import UserSidebar from "./UserSidebar";
 import UserContent from "./UserContent";
+import Profile from "./Profile"; // Add this import
 import "./UserHomePage.css";
 
 function UserHomePage() {
   return (
-    <>
-      <div className="dashboard">
-        <UserSidebar />
-        <div className="dashboard--content">
-          <UserContent />
-        </div>
+    <div className="dashboard">
+      <UserSidebar />
+      <div className="dashboard--content">
+        <UserContent>
+          <Profile /> {/* Profile component is now imported and used here */}
+        </UserContent>
       </div>
-    </>
+    </div>
   );
 }
 

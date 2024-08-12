f4d752db2c176886d843f492f2e8cc8d6783df16
lowellteddy <lowellteddy88@gmail.com>
2024-08-12 12:07:14 +0300

    added styling to the user profile

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
             element={
               <>
                 {" "}
+                
                 <Hero />
                 <Hservice />
                 <HAbout />

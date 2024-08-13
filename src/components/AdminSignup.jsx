b6dfd27c629d4266e3048a92b2270dbeaea7a80a
kelvin <kelvinmutugi336@gmil.com>
2024-08-13 10:26:22 +0300

    removes comments

--- src/components/AdminSignup.jsx
+++ src/components/AdminSignup.jsx
@@ -1,143 +1,8 @@
-// import React, { useState, useEffect } from "react";
-// import { useFormik } from "formik";
-// import * as Yup from "yup";
-// import { Link, useNavigate } from "react-router-dom";
-// import './AdminSignup.css';
-
-// function AdminSignup() {
-//   const [message, setMessage] = useState("");
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     return () => setMessage("");
-//   }, []);
-
-//   const formik = useFormik({
-//     initialValues: {
-//       username: "",
-//       email: "",
-//       password: "",
-//       password2: "",
-//     },
-//     validationSchema: Yup.object({
-//       username: Yup.string().required("Username is required"),
-//       email: Yup.string()
-//         .email("Invalid email address")
-//         .required("Email is required"),
-//       password: Yup.string().required("Password is required"),
-//       password2: Yup.string()
-//         .oneOf([Yup.ref("password"), null], "Passwords must match")
-//         .required("Confirm password is required"),
-//     }),
-//     onSubmit: async (values, { resetForm }) => {
-//       const response = await fetch(
-//         "http://127.0.0.1:5555/admin_auth/register",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify(values),
-//         }
-//       );
-
-//       const data = await response.json();
-//       setMessage(data.msg);
-
-//       if (response.ok) {
-//         resetForm();
-//         navigate("/logins");
-//       }
-//     },
-//   });
-
-//   return (
-//     <div>
-//       <h1>Admin Signup</h1>
-//       <form onSubmit={formik.handleSubmit}>
-//         <input
-//           id="username"
-//           name="username"
-//           type="text"
-//           placeholder="Enter Username"
-//           required
-//           autoComplete="username"
-//           onChange={formik.handleChange}
-//           onBlur={formik.handleBlur}
-//           value={formik.values.username}
-//         />
-//         {formik.touched.username && formik.errors.username ? (
-//           <div>{formik.errors.username}</div>
-//         ) : null}
-//         <br />
-
-//         <input
-//           id="email"
-//           name="email"
-//           type="email"
-//           placeholder="Enter Email"
-//           required
-//           autoComplete="email"
-//           onChange={formik.handleChange}
-//           onBlur={formik.handleBlur}
-//           value={formik.values.email}
-//         />
-//         {formik.touched.email && formik.errors.email ? (
-//           <div>{formik.errors.email}</div>
-//         ) : null}
-//         <br />
-
-//         <input
-//           id="password"
-//           name="password"
-//           type="password"
-//           placeholder="Enter Password"
-//           required
-//           autoComplete="new-password"
-//           onChange={formik.handleChange}
-//           onBlur={formik.handleBlur}
-//           value={formik.values.password}
-//         />
-//         {formik.touched.password && formik.errors.password ? (
-//           <div>{formik.errors.password}</div>
-//         ) : null}
-//         <br />
-
-//         <input
-//           id="password2"
-//           name="password2"
-//           type="password"
-//           placeholder="Confirm Password"
-//           required
-//           autoComplete="new-password"
-//           onChange={formik.handleChange}
-//           onBlur={formik.handleBlur}
-//           value={formik.values.password2}
-//         />
-//         {formik.touched.password2 && formik.errors.password2 ? (
-//           <div>{formik.errors.password2}</div>
-//         ) : null}
-
-//         <br />
-//         <button type="submit" disabled={!formik.isValid}>
-//           Sign Up
-//         </button>
-//       </form>
-//       {message && <p>{message}</p>}
-//       <div>
-//         <Link to="/logins">Already have an Account? Login here</Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AdminSignup;
-
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
-import './AdminSignup.css';
+import "./AdminSignup.css";
 
 function AdminSignup() {
   const [message, setMessage] = useState("");
@@ -258,7 +123,11 @@ function AdminSignup() {
         ) : null}
 
         <br />
-        <button type="submit" className="admin-signup-button" disabled={!formik.isValid}>
+        <button
+          type="submit"
+          className="admin-signup-button"
+          disabled={!formik.isValid}
+        >
           Sign Up
         </button>
       </form>
